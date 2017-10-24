41dfb8fde160d9a27f4154103f31f9be9ecf7b69
A² <32441938+a2monkey@users.noreply.github.com>
2017-10-23 20:40:30 -0700

    Added logging messages and fixed what needed fixing.

--- bot.py
+++ bot.py
@@ -11,13 +11,14 @@ driver = webdriver.Firefox(firefox_profile=profile)
 
 voyage_link = config['voyage']
 driver.get(voyage_link) #Visits the landing page
+message('Made a request [Voyage Landing Page] -> {}'.format(voyage_link))
 
 voyage_info = driver.find_element_by_class_name('md-hidden') #Finds the Select Room button
 voyage_info.click()
 
 session_link = driver.current_url
 
-message(session_link)
+message('New session link created -> {}'.format(session_link))
 
 guests = config['guests']
 guests_validator = re.match('^[-+]?[0-4]+$', guests)
@@ -33,9 +34,13 @@ time.sleep(5)
 
 element = driver.find_element_by_xpath('//section[@id="stateroom-meta"]').get_attribute('innerHTML')
 stateroom_element = htmlmin.minify(element, remove_empty_space=True)
+message('Analyzing State Room data.')
 
 if stateroom_element.count('Sold Out') == 5:
 	message('Analyzing Data Finished -> All Rooms Sold Out')
 else:
 	message('Analyzing Data Finished -> A Room is Available')
-	#TODO Notify
+
+message('Running again -> -10 mins.')
+time.sleep(600)
+os.system('python3 launcher.py')
